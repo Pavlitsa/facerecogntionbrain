0876b6e23f5d5ff11069ea829dd2f84ba3055977
Pavlitsa <pzafeiri@gmail.com>
2020-04-28 02:07:52 +0200

    code review

--- src/App.js
+++ src/App.js
@@ -28,11 +28,8 @@ const particlesOptions = {
   },
 };
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '',
+const initialState = {
+  input: '',
       imageUrl: '',
       box: {},
       route: 'signin',
@@ -44,8 +41,13 @@ class App extends Component {
         entries: 0,
         joined: ''
       }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
     }
-  }
 
   // Loading the user when registering
   loadUser = (data) => {
@@ -107,6 +109,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count}))
             })
+            .catch(console.log)
 
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
@@ -117,7 +120,7 @@ class App extends Component {
   // Defining the route depending on where we are (signed in, signed out etc)
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
